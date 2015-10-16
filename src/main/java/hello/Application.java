a39a4323a9e20ce799ed445e072aa0e61720ed21
Oleh Krupenia <oleh.krupenia@globallogic.com>
2015-10-15 20:10:13 -0400

    adding of the sources from Gradle project

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,22 @@
+package hello;
+
+/**
+ * Created by oleh.krupenia on 10/13/2015.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
